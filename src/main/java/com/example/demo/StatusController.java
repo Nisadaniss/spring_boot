cbf39527dfbcac8cd40737eb33ee09080b067cd5
Nisa Danis <nisadaniss@example.com>
2026-04-16 09:59:28 +0300

    son güncelleme

--- src/main/java/com/example/demo/StatusController.java
+++ src/main/java/com/example/demo/StatusController.java
@@ -9,6 +9,6 @@ public class StatusController {
 
     @GetMapping("/api/status")
     public String getStatus() {
-        return "Sistem Güncellendi: " + LocalDateTime.now() + " - CI/CD Adımları Başladı.";
+        return "Sistem Güncellendi aktif: " + LocalDateTime.now() + " - CI/CD Adımları Başladı.";
     }
 }
